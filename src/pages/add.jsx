0abc22eb271fded3a53817faf6fab8e2be8c310f
pmfoysal <pmfoysal@gmail.com>
2023-03-23 17:26:20 +0600

    Add Page Component Added

--- src/pages/add.jsx
+++ src/pages/add.jsx
@@ -1,3 +1,12 @@
+import Form from '../components/form';
+
 export default function Add() {
-   return <div>Add</div>;
+   return (
+      <div class='container relative'>
+         <main class='relative z-20 max-w-3xl mx-auto rounded-lg xl:max-w-none'>
+            <h1 class='mt-4 mb-8 text-3xl font-bold text-center text-gray-800'>Create Task for Your Team</h1>
+            <Form mode='add' />
+         </main>
+      </div>
+   );
 }
