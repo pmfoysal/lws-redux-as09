6c4a0278bcd9acc8c355913f63d7085875737419
pmfoysal <pmfoysal@gmail.com>
2023-03-23 17:27:28 +0600

    Edit Page Component Added

--- src/pages/edit.jsx
+++ src/pages/edit.jsx
@@ -1,3 +1,12 @@
+import Form from '../components/form';
+
 export default function Edit() {
-   return <div>Edit</div>;
+   return (
+      <div class='container relative'>
+         <main class='relative z-20 max-w-3xl mx-auto rounded-lg xl:max-w-none'>
+            <h1 class='mt-4 mb-8 text-3xl font-bold text-center text-gray-800'>Edit a Task</h1>
+            <Form mode='edit' />
+         </main>
+      </div>
+   );
 }
