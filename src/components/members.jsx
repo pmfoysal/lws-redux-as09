8ce2de08b65b86695d6a7bb934832d31174d661d
pmfoysal <pmfoysal@gmail.com>
2023-03-23 17:15:52 +0600

    Members Component Added

---
+++ src/components/members.jsx
@@ -0,0 +1,37 @@
+export default function Members() {
+   return (
+      <div class='mt-8'>
+         <h3 class='text-xl font-bold'>Team Members</h3>
+         <div class='mt-3 space-y-4'>
+            <div class='checkbox-container'>
+               <img src='./images/avatars/sumit.png' class='team-avater' />
+               <p class='label'>Sumit Saha</p>
+            </div>
+            <div class='checkbox-container'>
+               <img src='./images/avatars/sadh.png' class='team-avater' />
+               <p class='label'>Sadh Hasan</p>
+            </div>
+            <div class='checkbox-container'>
+               <img src='./images/avatars/akash.png' class='team-avater' />
+               <p class='label'>Akash Ahmed</p>
+            </div>
+            <div class='checkbox-container'>
+               <img src='./images/avatars/salahuddin.png' class='team-avater' />
+               <p class='label'>Md Salahuddin</p>
+            </div>
+            <div class='checkbox-container'>
+               <img src='./images/avatars/riyadh.png' class='team-avater' />
+               <p class='label'>Riyadh Hassan</p>
+            </div>
+            <div class='checkbox-container'>
+               <img src='./images/avatars/ferdous.png' class='team-avater' />
+               <p class='label'>Ferdous Hassan</p>
+            </div>
+            <div class='checkbox-container'>
+               <img src='./images/avatars/almas.png' class='team-avater' />
+               <p class='label'>Arif Almas</p>
+            </div>
+         </div>
+      </div>
+   );
+}
