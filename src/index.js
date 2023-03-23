34ef3e1ede242ae9686a4e45063102a3a049d352
pmfoysal <pmfoysal@gmail.com>
2023-03-23 17:29:59 +0600

    App UI Styles Added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import App from './app/app';
+import './styles/index.css';
+import './styles/custom.css';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
    <React.StrictMode>
-      <App />
+      <Provider store={store}>
+         <BrowserRouter>
+            <App />
+         </BrowserRouter>
+      </Provider>
    </React.StrictMode>
 );
