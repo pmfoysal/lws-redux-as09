f64fb6a970c27f4ebcb01e4778e2ac775bae3cbb
pmfoysal <pmfoysal@gmail.com>
2023-03-25 00:52:24 +0600

    Tasks Filter Functionality Added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,32 @@
 import Task from '../components/task';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Sidebar from '../components/sidebar';
 import { useGetTasksQuery } from '../redux/features/tasks';
 
 export default function Home() {
    const { data: tasks } = useGetTasksQuery();
+   const { search, members, projects } = useSelector(store => store.others);
+
+   function tasksSearch(item) {
+      if (!search) return true;
+      const name = item.taskName.toLowerCase();
+      // const member = item.teamMember.name.toLowerCase();
+      // const project = item.project.projectName.toLowerCase();
+      if (name.includes(search.toLowerCase())) return true;
+      // else if (member.includes(search.toLowerCase())) return true;
+      // else if (project.includes(search.toLowerCase())) return true;
+      return false;
+   }
+
+   function tasksMembers(item) {
+      if (!members.length) return true;
+      return members.includes(item.teamMember.id);
+   }
+
+   function tasksProjects(item) {
+      return projects.includes(item.project.id);
+   }
 
    return (
       <div className='container relative'>
@@ -27,9 +49,13 @@ export default function Home() {
                   </Link>
                </div>
                <div className='lws-task-list'>
-                  {tasks?.map((item, index) => (
-                     <Task key={`task-${index}`} data={item} />
-                  ))}
+                  {tasks
+                     ?.filter(tasksMembers)
+                     ?.filter(tasksProjects)
+                     ?.filter(tasksSearch)
+                     ?.map((item, index) => (
+                        <Task key={`task-${index}`} data={item} />
+                     ))}
                </div>
             </main>
          </div>
