ee91712c2984d86ec1bf95c936fe27f1334c242e
pmfoysal <pmfoysal@gmail.com>
2023-03-24 16:10:18 +0600

    App Layout Component Added

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,3 +1,17 @@
+import Add from '../pages/add';
+import Edit from '../pages/edit';
+import Home from '../pages/home';
+import Layout from '../layouts/layout';
+import { Route, Routes } from 'react-router-dom';
+
 export default function App() {
-   return <div>App</div>;
+   return (
+      <Routes>
+         <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/add' element={<Add />} />
+            <Route path=':taskId/edit' element={<Edit />} />
+         </Route>
+      </Routes>
+   );
 }
