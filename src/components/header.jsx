931969c7f817f40a5caed0ecdcb09fc7d043b325
pmfoysal <pmfoysal@gmail.com>
2023-03-25 00:39:33 +0600

    App Search Functionality Added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom';
+import { setSearch } from '../redux/features/others';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Header() {
+   const dispatch = useDispatch();
+   const search = useSelector(store => store.others.search);
+
    return (
       <nav className='container relative py-3'>
          <div className='flex items-center justify-between'>
@@ -9,7 +14,14 @@ export default function Header() {
             </Link>
             <div className='flex-1 max-w-xs search-field group'>
                <i className='fa-solid fa-magnifying-glass search-icon group-focus-within:text-blue-500'></i>
-               <input type='text' placeholder='Search Task' className='search-input' id='lws-searchTask' />
+               <input
+                  type='text'
+                  placeholder='Search Task'
+                  className='search-input'
+                  id='lws-searchTask'
+                  value={search}
+                  onChange={e => dispatch(setSearch(e.target.value))}
+               />
             </div>
          </div>
       </nav>
